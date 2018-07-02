bdc0c0e835b6d27d1980355bd9fa1bd11a3b6178
chenxianxi <576729125@qq.com>
2018-07-02 17:38:24 +0800

    recommend

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default{
+  data () {
+    return {
+      msg: 'this is my home.'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/toplist/TopList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default{
+  data () {
+    return {
+      msg: 'this is top list page.'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
